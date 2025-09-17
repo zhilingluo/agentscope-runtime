bca877b04a502d91334ddb3abb3c725d99475e65
Weirui Kuang <39145382+rayrayraykk@users.noreply.github.com>
2025-09-17 17:34:47 +0800

    [Version] Bump version to 0.1.5 (#77)

--- cookbook/build.sh
+++ cookbook/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Multiple versions to build
-VERSIONS=("preview" "v0.1.0" "v0.1.1" "v0.1.2" "v0.1.3")
+VERSIONS=("preview" "v0.1.0" "v0.1.1" "v0.1.2" "v0.1.3" "v0.1.4")
 OUTPUT_DIR="_build"
 
 # ANSI color codes for better display
--- src/agentscope_runtime/version.py
+++ src/agentscope_runtime/version.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-__version__ = "v0.1.4"
+__version__ = "v0.1.5"
